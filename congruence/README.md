4c622a8b6a074facee3b24375cc36ce742c67dcf
Lynette Mwangi <lynettemwangi@Lynettes-MacBook-Pro.local>
2024-05-05 23:46:58 +0300

    chore: Linear congruence

---
+++ congruence/README.md
@@ -0,0 +1,5 @@
+# Linear Congruence
+
+This is my implementation of Linear Congruence based on the following question:
+
+Write a program that solves the congruence ax = c(mod m). If gcd(a,m) does not divide c, return an error message and the value of gcd(a,m)
---
+++ congruence/src/main.rs
@@ -0,0 +1,52 @@
+fn main() {
+    fn congruence(mut a: i32, mut c: i32, m: i32) -> i32 {
+        a = a % m;
+        c = c % m;
+        // let u = 0;
+        // let v = 0;
+        let (d, u, v) = extendedeuclidean(a, m);
+        if c % d == 0 {
+            let x0 = (u * (c / d)) % m;
+            if x0 < 0 {
+                return x0 + m;
+            } else {
+                return x0;
+            }
+        } else {
+            return -1;
+        }
+    }
+
+    fn extendedeuclidean(mut a: i32, mut b: i32) -> (i32, i32, i32) {
+        // The extended Euclidean algorithm will be used to find the gcd of a and m
+        let mut q = 0;
+        let mut r = 1;
+        let mut x1 = 1;
+        let mut x2 = 0;
+        let mut x3 = 1;
+        let mut y1 = 0;
+        let mut y2 = 1;
+        let mut y3 = 0;
+
+        while r != 0 && b > 0{
+            q = a / b;
+            r = a % b;
+            x3 = x1 - q * x2;
+            y3 = y1 - q * y2;
+
+            if r != 0 {
+                a = b;
+                b = r;
+                x1 = x2;
+                x2 = x3;
+                y1 = y2;
+                y2 = y3;
+            }
+        }
+
+        return(b, x2, y2)
+    }
+
+    let x = congruence(15, 9, 57);
+    println!("{}", x);
+}
